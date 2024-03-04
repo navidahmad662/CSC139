3befaa5b3a3c41c3f33091af964231673f2a5cbf
Navid Ahmad <navidahmad662@gmail.com>
2024-03-04 15:45:58 -0800

    added the preProcess

--- wish.c
+++ wish.c
@@ -88,9 +88,140 @@ int newProcess(char *myargs[]) {
 }
 
 // Function to preprocess the input command
-int preProcess(char *buffer) {
-    // Implementation for preprocessing input command
+int preProcess(char *buffer){
+        int stdout_copy=0;
+        int rc;
+        if(strstr(buffer,">")!=NULL){ //REDIRECT
+                        int a=0;
+                        
+                        char* multiRedirect[sizeof(char)*512];
+                        multiRedirect[0]= strtok(strdup(buffer)," \n\t>");
+                        while(multiRedirect[a]!=NULL){
+                                a++;
+                                multiRedirect[a]=strtok(NULL," \n\t>");
+                        }
+                        if(a==1){ //no output file
+                            write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE)); 
+                            exit(0);    
+                        }
+                        int i=0;
+                        char* myargs[sizeof(buffer)];
+                        myargs[0]= strtok(buffer,"\n\t>");
+                        while(myargs[i]!=NULL){
+                                i++;
+                                myargs[i]=strtok(NULL," \n\t>"); 
+                        }
+                        if(i>2){ //no output file
+                            write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE)); 
+                            exit(0);    
+                        }
+                        int x=0;
+                        char* tokenize[sizeof(myargs[1])];
+                        tokenize[0]= strtok(myargs[1]," \n\t");
+                        while(tokenize[x]!=NULL){
+                                x++;
+                                tokenize[x]=strtok(NULL," \n\t"); 
+                        }
+                        
+                        char *fout=strdup(tokenize[0]);
+                        stdout_copy=dup(1);
+                        int out=open(fout,O_WRONLY|O_CREAT|O_TRUNC,0666);
+                        int error=open(fout,O_WRONLY|O_CREAT|O_TRUNC,0666);
+                        fflush(stdout);
+                        dup2(out,STDOUT_FILENO);
+                        dup2(out,STDERR_FILENO);
+                        close(out);
+                        CLOSED=1;
+                        if(out==-1 || error==-1 || x>1 || i>2){
+                                write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+                                exit(0);
+                        }
+                        myargs[i+1]=NULL;
+                        tokenize[x+1]=NULL;
+                        strcpy(buffer,myargs[0]);
+                        
+                        
+                }
+                
+                if(buffer[0] != '\0' && buffer[0] != '\n') {
+                        char *command[sizeof(buffer)];
+                        command[0] = strtok(buffer, " \t\n");
+                        int p=0;
+                        while(command[p]!=NULL){
+                                p++;
+                                command[p]=strtok(NULL, " \n\t");
+                                
+                        }
+                        command[p+1]=NULL;
+		        if(strcmp(command[0],"cd") == 0){//cd
+                                if(p==2){
+                                        if(chdir(command[1])!=0){
+                                                write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+                                               
+                                        }
+                                 }
+                                 else{ //0 Arguments or more than 2 arguments?
+                                        write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+                                        
+                                 }
+                                                        
+                        }  
+                        else if(strcmp(command[0],"path") == 0){
+                                pathChanged=1;
+                                if(p==2){
+                                        pathEmpty=0;
+                                        path=strdup(command[1]);
+                                         if(path[strlen(path)-1]!='/'){
+                                                strcat(path,"/");
+                                        }      
+                                }
+                                else if(p==1){
+                                       
+                                        pathEmpty=1;
+                                }
+                                else{ 
+                                        pathEmpty=0;
+                                        for(int i=1;i<p;i++){
+                                                char *temp=strdup(command[i]);
+                                                if(temp[strlen(temp)-1]!='/')
+                                                        strcat(temp,"/");
+                                                strcpy(multiPath[i-1],temp);
+                                                numberMultiPath++;
+                                        }
+                                        
+                                        //printf("%d\n",numberMultiPath);
+                                        //for(int i=0;i<numberMultiPath;i++)
+                                                //printf("%s\n",multiPath[i]);
+                        
+                                }
+                                
+                                       
+			}
+                        else if(strcmp(command[0],"exit") == 0) {
+			    if(p==1){
+                                        exit(0);
+                                }
+                                else{ //0 Arguments or more than 2 arguments?
+                                        write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+                                        
+                                }
+                        }    
+                        else{
+                                if(pathEmpty==1)
+                                        write(STDERR_FILENO, ERROR_MESSAGE, strlen(ERROR_MESSAGE));
+                                else
+                                        rc=newProcess(command);
+                        }
+
+                }
+                if(CLOSED==1){
+                        dup2(stdout_copy,1);
+                        close(stdout_copy);
+                        
+                }
+               return rc;
 }
+
 
 int main(int argc, char* argv[]) {
     FILE *file = NULL;
